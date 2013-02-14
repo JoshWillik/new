a93b13b36c3f19b96432a2e54aeb5062bf7cc201
Josh(Desktop) <joshwillik@gmail.com>
2013-02-14 07:15:40 -0500

    Fix bug that caused script to halt on folders with spaces in titles

--- new.sh
+++ new.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 if [ ! -d ~/.new ] 
 then
     mkdir ~/.new
@@ -11,9 +13,11 @@ fi
 for folder in $@
 do
     TOTAL="$TOTAL/$folder"
+    echo $TOTAL
 done    
+
 
 rm -rf ~/.new/.tmp/* #clear temp
 cp -R ~/.new$TOTAL/* ~/.new/.tmp #copy to intermediate location
 find ~/.new/.tmp -type f -name ".*" -delete #strip of hidden files/directories
-cp -R ~/.new/.tmp/* $PWD #copy stripped version of folder
+cp -R ~/.new/.tmp/* "$PWD" #copy stripped version of folder
